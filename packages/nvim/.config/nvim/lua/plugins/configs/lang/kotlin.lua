06cf5c8d57be3c1f7cd1bad3bd51db59860421e2
ryugen04 <yamat.2667@gmail.com>
2025-12-22 14:45:25 +0900

    feat: change kotlin :sparkles:

--- packages/nvim/.config/nvim/lua/plugins/configs/lang/kotlin.lua
+++ packages/nvim/.config/nvim/lua/plugins/configs/lang/kotlin.lua
@@ -8,6 +8,9 @@ return {
 		config = function()
 			require("kotlin-extended-lsp").setup({
 				debug_init_options = true,
+				-- VSCode相当: --system-path オプション（キャッシュ・インデックス保存先）
+				system_path = vim.fn.expand("~/Library/Caches/kotlin-lsp"),
+				-- initializationOptions.cacheDirectory
 				cache_directory = vim.fn.expand("~/Library/Caches/kotlin-lsp"),
 				env = {
 					GITHUB_TOKEN = os.getenv("GITHUB_TOKEN"),
